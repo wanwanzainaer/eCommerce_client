86b1e949ca5ee67f0d9fcc25644f022cbd950463
simon <wanwanzainaer@gmail.com>
2021-01-11 22:25:47 -0800

    Fixed the key issuse in homescreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -32,8 +32,8 @@ const HomeScreen = () => {
         <Row>
           {products.map((product: IProduct) => {
             return (
-              <Col sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} key={product._id} />
+              <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                <Product product={product} />
               </Col>
             );
           })}
